9a68d1a4598b2d7180461ded72246e00ead7b3ad
simon <simonetchegno@gmail.com>
2024-02-08 14:01:35 -0300

    cambios 8/02

---
+++ pagina c3i/main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.containerInformacion');
+ScrollReveal().reveal('.containerMarcasBG',{ delay:500});
+ScrollReveal().reveal('.services1',{ delay:500});
+ScrollReveal().reveal('.services2',{ delay:700});
+ScrollReveal().reveal('.citaContainer',{ delay:500});
+ScrollReveal().reveal('.c',{ delay:800});
+ScrollReveal().reveal('.containerFooter',{ delay:300});
+
+
