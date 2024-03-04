864f473581dc09aa98fed3afdfbb42b96eb9ba79
Simón Etchegno <88148174+SimonEtchegno@users.noreply.github.com>
2024-03-04 13:26:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-ScrollReveal().reveal('.containerInformacion');
+ScrollReveal().reveal('.principiosH2',{ delay:700});
+ScrollReveal().reveal('.pInformacion',{ delay:1000});
+ScrollReveal().reveal('.imgSection',{ delay:1000});
 ScrollReveal().reveal('.containerMarcasBG',{ delay:500});
-ScrollReveal().reveal('.services1',{ delay:500});
-ScrollReveal().reveal('.services2',{ delay:700});
+ScrollReveal().reveal('.tituloRazon',{ delay:900});
+ScrollReveal().reveal('.services1',{ delay:1000});
+ScrollReveal().reveal('.services2',{ delay:1100});
 ScrollReveal().reveal('.citaContainer',{ delay:500});
 ScrollReveal().reveal('.c',{ delay:800});
-ScrollReveal().reveal('.containerFooter',{ delay:300});
-
 
 
 
